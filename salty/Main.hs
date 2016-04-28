cdca146a357bc47277619f19f4ac611946b64b9b
Trevor Elliott <trevor@galois.com>
2016-04-28 15:18:48 -0700

    Warning removal

--- salty/Main.hs
+++ salty/Main.hs
@@ -3,12 +3,14 @@ import Scope.Check
 import Syntax.Lexer
 import Syntax.Parser
 
-import qualified Data.Text.Lazy as L
 import qualified Data.Text.Lazy.IO as L
 
-main =
+main :: IO ()
+main  =
   do bytes <- L.readFile "test.salt"
      mapM_ print (lexWithLayout "test.salt" bytes)
      let Right cont = parseController "test.salt" bytes
 
-     print (scopeCheck cont)
+     let scCont = scopeCheck cont
+
+     print scCont
