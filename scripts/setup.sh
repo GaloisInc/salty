896e8fcfb68136ccfd10234b09bc4ee3ec6d2718
Trevor Elliott <trevor@galois.com>
2016-08-15 21:27:20 -0700

    Better caching for the travis build

---
+++ scripts/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ev
+
+# Setup ~/.local/bin
+if [ ! -d ~/.local/bin ]; then
+    mkdir -p ~/.local/bin
+fi
+
+if [ ! -f ~/.local/bin/stack ]; then
+    curl -L https://www.stackage.org/stack/linux-x86_64 \
+      | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+fi
+
+if [ ! -h ~/.local/bin/g++ ]; then
+    ln -s /usr/bin/g++-4.9 ~/.local/bin/g++
+fi
+
+export PATH=$HOME/.local/bin:$PATH
+
+if [ ! -d ~/slugs-master/src ]; then
+    curl -LO https://github.com/VerifiableRobotics/slugs/archive/master.zip
+    unzip master.zip -d ~
+    rm master.zip
+fi
+
+if [ ! -f ~/slugs-master/src/slugs ]; then
+    (cd ~/slugs-master/src && make)
+fi
